7782a8564bb2c7e160694161abf1ee29b23e232c
Arthur Campos <138174613+arthurd3@users.noreply.github.com>
2024-09-20 13:06:01 -0300

    C# (VETOR)

---
+++ VETOR/Program.cs
@@ -0,0 +1,36 @@
+﻿// Escreva um programa que leia um vetor de 15 números inteiros e exiba ao final apenas os números que estão armazenados nas posições pares do vetor.
+using System;
+
+class Vetor{
+
+    static int[] lerVet (int[] vet , int i){
+
+        while(i < vet.Length){
+            Console.WriteLine($"Digite Um Numero : {i +1} / {vet.Length}");
+            vet[i] = Convert.ToInt32(Console.ReadLine());
+            i++;
+        }
+        return vet;
+    }
+    static void ImprimirVet (int[] vet ){
+
+        for (int i = 0 ; i < vet.Length ; i++){
+            Console.WriteLine($"Vetor Preenchido : {vet[i],15} | {i+1,12}/{vet.Length}");
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        
+        int[] num = new int[15];
+        int i=0;
+
+        lerVet(num , i);
+        ImprimirVet(num);
+
+    }
+
+
+
+}
